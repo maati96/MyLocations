0a8831e29489b7fc0411c975833e7f060ee4b260
Mohamed Maaty <m.maaty969@gmail.com>
2018-12-10 23:52:48 +0200

    Initial Commit

---
+++ MyLocations/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  MyLocations
+//
+//  Created by Mohamed Maati on 12/10/18.
+//  Copyright © 2018 Mohamed Maati. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
